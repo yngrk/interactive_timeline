34508ee72f52b710e4364e49efc48349f7ca41cc
yngrk <yngrk.kim@gmail.com>
2025-01-03 18:21:54 +0100

    tailwind setup

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main>
+    <h1 class="text-5xl">
+      Hello world
+    </h1>
+  </main>
 </template>
+
+<style>
+:root {
+  @apply bg-gray-900 text-white
+}
+</style>
